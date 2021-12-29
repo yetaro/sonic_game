42db67c4589cc20c50476f2ee46d0a11e8987765
Jose Luis <jlml.04@hotmail.com>
2021-12-29 16:41:11 -0600

    subida de proyecto

---
+++ prueba_sprites/level2.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace prueba_sprites
+{
+    partial class level2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.piso_1 = new System.Windows.Forms.PictureBox();
+            this.piso_2 = new System.Windows.Forms.PictureBox();
+            this.piso_3 = new System.Windows.Forms.PictureBox();
+            this.piso_4 = new System.Windows.Forms.PictureBox();
+            this.piso_5 = new System.Windows.Forms.PictureBox();
+            this.piso_6 = new System.Windows.Forms.PictureBox();
+            this.sonic_1 = new System.Windows.Forms.PictureBox();
+            this.suelo2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.esmeraldas_1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sonic_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.suelo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.esmeraldas_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // piso_1
+            // 
+            this.piso_1.BackColor = System.Drawing.Color.Transparent;
+            this.piso_1.Image = global::prueba_sprites.Properties.Resources._1574631029696;
+            this.piso_1.Location = new System.Drawing.Point(13, 260);
+            this.piso_1.Name = "piso_1";
+            this.piso_1.Size = new System.Drawing.Size(137, 130);
+            this.piso_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_1.TabIndex = 0;
+            this.piso_1.TabStop = false;
+            // 
+            // piso_2
+            // 
+            this.piso_2.Image = global::prueba_sprites.Properties.Resources._1574993854299;
+            this.piso_2.Location = new System.Drawing.Point(259, 200);
+            this.piso_2.Name = "piso_2";
+            this.piso_2.Size = new System.Drawing.Size(87, 18);
+            this.piso_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_2.TabIndex = 1;
+            this.piso_2.TabStop = false;
+            // 
+            // piso_3
+            // 
+            this.piso_3.BackColor = System.Drawing.Color.Transparent;
+            this.piso_3.Image = global::prueba_sprites.Properties.Resources._1574993854299;
+            this.piso_3.Location = new System.Drawing.Point(477, 302);
+            this.piso_3.Name = "piso_3";
+            this.piso_3.Size = new System.Drawing.Size(66, 156);
+            this.piso_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_3.TabIndex = 2;
+            this.piso_3.TabStop = false;
+            // 
+            // piso_4
+            // 
+            this.piso_4.Image = global::prueba_sprites.Properties.Resources._1574993854299;
+            this.piso_4.Location = new System.Drawing.Point(713, 380);
+            this.piso_4.Name = "piso_4";
+            this.piso_4.Size = new System.Drawing.Size(56, 78);
+            this.piso_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_4.TabIndex = 3;
+            this.piso_4.TabStop = false;
+            // 
+            // piso_5
+            // 
+            this.piso_5.Image = global::prueba_sprites.Properties.Resources._1574993854299;
+            this.piso_5.Location = new System.Drawing.Point(926, 169);
+            this.piso_5.Name = "piso_5";
+            this.piso_5.Size = new System.Drawing.Size(91, 18);
+            this.piso_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_5.TabIndex = 4;
+            this.piso_5.TabStop = false;
+            // 
+            // piso_6
+            // 
+            this.piso_6.BackColor = System.Drawing.Color.Transparent;
+            this.piso_6.Image = global::prueba_sprites.Properties.Resources._1574631029696;
+            this.piso_6.Location = new System.Drawing.Point(1157, 272);
+            this.piso_6.Name = "piso_6";
+            this.piso_6.Size = new System.Drawing.Size(118, 118);
+            this.piso_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piso_6.TabIndex = 5;
+            this.piso_6.TabStop = false;
+            // 
+            // sonic_1
+            // 
+            this.sonic_1.BackColor = System.Drawing.Color.Transparent;
+            this.sonic_1.Location = new System.Drawing.Point(13, 136);
+            this.sonic_1.Name = "sonic_1";
+            this.sonic_1.Size = new System.Drawing.Size(36, 34);
+            this.sonic_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.sonic_1.TabIndex = 6;
+            this.sonic_1.TabStop = false;
+            // 
+            // suelo2
+            // 
+            this.suelo2.BackColor = System.Drawing.Color.Transparent;
+            this.suelo2.Location = new System.Drawing.Point(92, 396);
+            this.suelo2.Name = "suelo2";
+            this.suelo2.Size = new System.Drawing.Size(1075, 10);
+            this.suelo2.TabIndex = 7;
+            this.suelo2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 50);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "RETRY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // esmeraldas_1
+            // 
+            this.esmeraldas_1.BackColor = System.Drawing.Color.Transparent;
+            this.esmeraldas_1.Image = global::prueba_sprites.Properties.Resources.esmeraldas;
+            this.esmeraldas_1.Location = new System.Drawing.Point(1186, 144);
+            this.esmeraldas_1.Name = "esmeraldas_1";
+            this.esmeraldas_1.Size = new System.Drawing.Size(78, 50);
+            this.esmeraldas_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.esmeraldas_1.TabIndex = 10;
+            this.esmeraldas_1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1157, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 76);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "SCORE";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // level2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::prueba_sprites.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1276, 458);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.esmeraldas_1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sonic_1);
+            this.Controls.Add(this.piso_6);
+            this.Controls.Add(this.piso_5);
+            this.Controls.Add(this.piso_4);
+            this.Controls.Add(this.piso_3);
+            this.Controls.Add(this.piso_2);
+            this.Controls.Add(this.piso_1);
+            this.Controls.Add(this.suelo2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "level2";
+            this.Text = "level2";
+            this.Load += new System.EventHandler(this.level2_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.level2_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.level2_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.piso_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piso_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sonic_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.suelo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.esmeraldas_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox piso_1;
+        private System.Windows.Forms.PictureBox piso_2;
+        private System.Windows.Forms.PictureBox piso_3;
+        private System.Windows.Forms.PictureBox piso_4;
+        private System.Windows.Forms.PictureBox piso_5;
+        private System.Windows.Forms.PictureBox piso_6;
+        private System.Windows.Forms.PictureBox sonic_1;
+        private System.Windows.Forms.PictureBox suelo2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox esmeraldas_1;
+        private System.Windows.Forms.Button button2;
+    }
+}
